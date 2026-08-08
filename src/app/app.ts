e0341b9d23f34b62d6eb13dc673b00f2cc3ebb27
MariamKanchashvili <mariam.kanchashvili748@eab.tsu.edu.ge>
2026-08-08 16:19:25 +0400

    add footer -fully responsive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './core/layouts/header/header';
+import { Footer } from "./core/layouts/footer/footer";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
